eddca9bcafa8cfca31920cecb251f558663fa9c8
sufyaan <97327266+sufyn@users.noreply.github.com>
2022-08-28 10:38:00 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import pickle
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+data = pd.read_csv("database.csv")
+#data.columns
+
+
+data = data[['Latitude', 'Longitude', 'Depth', 'Magnitude']]
+#data.head()
+
+
+
+data = np.array(data)
+#print(data)
+X = data[:, 0:-1]
+y = data[:, -1]
+y = y.astype('int')
+X = X.astype('int')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#print(X_train,y_train)
+
+
+
+rfc = RandomForestClassifier()
+rfc.fit(X_train, y_train)
+#y_pred = rfc.predict(X_test)
+
+#print(metrics.accuracy_score(y_test, y_pred))
+pickle.dump(rfc,open('model.pkl','wb'))
